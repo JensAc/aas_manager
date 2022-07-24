cf07b452b93d7c52bd34d00b3240b99a13f93fdd
Jens Schneider <jens.schneider.ac@posteo.de>
2022-07-24 12:51:02 +0200

    Make mach-nix environment work
    Signed-off-by: Jens Schneider <jens.schneider.ac@posteo.de>

--- env/shell.nix
+++ env/shell.nix
@@ -1,11 +1,11 @@
 with (import ./inputs.nix);
-with import <nixpkgs> {};
-with libsForQt5;
 pkgs.mkShell {
   buildInputs = [
     (import ./python.nix)
     mach-nix.mach-nix
   ];
-  QT_QPA_PLATFORM_PLUGIN_PATH="${qt5.qtbase.bin}/lib/qt-${qt5.qtbase.version}/plugins";
+  nativeBuildIntputs = [ pkgs.libsForQt5.wrapQtAppsHook ];
+  QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.libsForQt5.qt5.qtbase.bin}/lib/qt-${pkgs.libsForQt5.qt5.qtbase.version}/plugins";
+  QT_XCB_GL_INTEGRATION="none";
   QT_DEBUG_PLUGINS=1;
 }
